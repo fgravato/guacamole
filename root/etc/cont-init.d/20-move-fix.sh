aeffb0314d03c3d1cec4d0ce1865bac280f68d86
Gregg Gehman <gregg@largenut.com>
2021-10-06 04:42:07 +0000

    Delete 'root/etc/cont-init.d/20-move-fix.sh'

--- root/etc/cont-init.d/20-move-fix.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv sh
-
-if [ -d "/bitnami/tomcat/webapps/guacamole" ]; then
-  mv /bitnami/tomcat/webapps/guacamole/* /app/guacamole
-  rm -rf /bitnami/tomcat/webapps/guacamole
-fi
